24284fa9c4d6c6138e61ff78f613fdf5bc30c563
jobytchacko <jobyttchacko@gmail.com>
2023-04-28 22:52:23 +0530

    Docker tried with springboot

---
+++ src/main/java/com/jc/springbootrest/model/Greeting.java
@@ -0,0 +1,29 @@
+package com.jc.springbootrest.model;
+
+
+public class Greeting {
+
+    public Greeting(long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    long id;
+    String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
